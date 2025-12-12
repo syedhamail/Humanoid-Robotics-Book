6bed2b8934e7780bd1e1cb35b6970da34fef6c93
Syed Hamail Mohi Uddin Qazi <hamailsyed139@gmail.com>
2025-12-12 19:57:17 +0500

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://syedhamail.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Humanoid-Robotics-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
