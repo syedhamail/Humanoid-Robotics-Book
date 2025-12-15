0eb63e9b231b794d2d9dde854e9982b1bebc138e
Syed Hamail Mohi Uddin Qazi <hamailsyed139@gmail.com>
2025-12-16 00:48:58 +0500

    first commit

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://syedhamail.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Humanoid-Robotics-Book/',
+  baseUrl: '/',
 
   organizationName: 'syedhamail',
   projectName: 'Humanoid-Robotics-Book',
